e43874fba9fc811ead0be5727b955a446281e3c2
Davi Costa <davim.costa@outlook.com>
2024-03-25 18:16:53 -0300

    home page

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import {
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+  ImYoutube
+} from 'react-icons/im'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return  <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href='https://www.facebook.com' target='__blank'> 
+          <ImFacebook />
+        </a>
+      </li>
+      <li>
+        <a href='https://www.twitter.com' target='__blank'> 
+          <ImTwitter />
+        </a>
+      </li>
+      <li>
+        <a href='https://www.pinterest.com' target='__blank'> 
+          <ImPinterest />
+        </a>
+      </li>
+      <li>
+        <a href='https://www.instagram.com' target='__blank'> 
+          <ImInstagram />
+        </a>
+      </li>
+      <li>
+        <a href='https://www.youtube.com' target='__blank'> 
+          <ImYoutube />
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import WomanImg from '../img/about/woman.png'
+
+import { Link } from 'react-router-dom'
+
 const About = () => {
-  return <div>About</div>;
+  return <section className='section'>
+    <div className='container mx-auto h-full relative'>
+        <div className='flex flex-col 
+            lg:flex-row h-full items-center justify-center 
+            gap-x-24 text-center lg:text-left lg:pt-16'>
+          <div className='flex-1 max-h-96 lg:max-h-max 
+          order-2 lg:order-none overflow-hidden'>
+            <img src={WomanImg}></img>
+          </div>
+          <div>
+            <h1 className='h1'>About me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue odio, fermentum vel venenatis quis, sollicitudin quis purus. Nulla aliquam ultrices ligula id posuere. Suspendisse dolor nibh.</p>
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue odio, fermentum vel venenatis quis, sollicitudin quis purus. Nulla aliquam ultrices ligula id posuere. Suspendisse dolor nibh.</p>
+            <Link to='/portfolio'>View my work</Link>
+          </div>
+        </div>
+    </div>
+  </section>;
 };
 
 export default About;
