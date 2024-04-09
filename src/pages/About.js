9441344a90f149836ee6ab0524958d21c4402ac6
Davi Costa <davim.costa@outlook.com>
2024-04-09 18:57:36 -0300

    correct height in about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -36,7 +36,7 @@ const About = () => {
               exit={{opacity: 0, y: '-80%'}}
               transition={transition1}
               className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center
-              items-center lg:items-start mx-7
+              items-center lg:items-start mx-7 mt-28
           '>
             <h1 className='h1'>About me</h1>
             <p className='mb-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque augue odio, fermentum vel venenatis quis, sollicitudin quis purus.
